e18ab5325d9e3f72a0403e8c59ce8d164d3f717e
SrikarGathey1 <venkatnaras123@gmail.com>
2025-01-12 00:45:25 +0530

    Commit of the day

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 
-// @desc    Fetch user statistics
+// @desc    Fetch user statistics like total articles, total words
 // @route   GET /api/users
 // @access  Public
 
