e8a698b9a6095254edd4dc401ebdf1e720ebfcaa
SrikarGathey1 <venkatnaras123@gmail.com>
2025-01-13 23:47:14 +0530

    .

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const router = express.Router();
 
 // @desc    Fetch user statistics like total articles, total words, articles left
 // @route   GET /api/users
 // @access  Public
+
 
 
 // @desc    Statistics about words user has interacted with
