d2440bb76308e67c3b6ec3037c0b3989384f9014
SrikarGathey1 <venkatnaras123@gmail.com>
2025-01-15 21:50:00 +0530

    commit of the day

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const UserWord = require('../models/userWordModel'); 
 
 // @desc    Fetch user statistics like total articles, total words, articles left
 // @route   GET /api/users
 // @access  Public
 router.get('/:id', async (req, res) => {
-    try{
+    try
+    {
+
+
 
     }
-    catch
+    catch (error)
     {
-        
+
     }
 
 });
@@ -20,8 +25,25 @@ router.get('/:id', async (req, res) => {
 // @desc    Statistics about words user has interacted with
 // @route   GET /api/users
 // @access  Public
+router.get('/word/:userId', async (req, res) => {
+    const { userId } = req.params;
+    try 
+    {
+        const wordResults = await UserWord.find({ userId });
+        res.status(200).json(wordResults.length);
+    }
+    catch (error) 
+    {
+        res.status(500).json({ message: 'Failed to fetch user statistics', error: error.message });
+    }
+});
 
 
 // @desc    Statistics about articles user has interacted with
 // @route   GET /api/users
 // @access  Public
+
+
+
+
+module.exports = router;
